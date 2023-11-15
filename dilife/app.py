d4027aceca6e12e77b83dff201d32c3e2d6b377e
Mateus Ribeiro <mateus.ribe.sva@gmail.com>
2023-11-15 14:52:01 -0300

    feature: implement create user route

--- dilife/app.py
+++ dilife/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from dilife.schemas import UserPublic, UserSchema
+
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Hello World'}
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
