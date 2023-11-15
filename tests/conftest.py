5b3e3c5017c99d902219e57a9d2a486b93b834c2
Mateus Ribeiro <mateus.ribe.sva@gmail.com>
2023-11-15 18:24:27 -0300

    tests: created test for CRUD API

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,43 @@
 from fastapi.testclient import TestClient
 from pytest import fixture
-from sqlalchemy import create_engine
+from sqlalchemy import StaticPool, create_engine
 from sqlalchemy.orm import sessionmaker
 
 from dilife.app import app
-from dilife.models import Base
-
-
-@fixture
-def client():
-    return TestClient(app)
+from dilife.database import get_session
+from dilife.models import Base, User
 
 
 @fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
     yield Session()
     Base.metadata.drop_all(engine)
+
+
+@fixture
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
+
+
+@fixture
+def user(session):
+    user = User(username='foo', email='foo@bar.com', password='secret123')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from dilife.schemas import UserPublic
+
+
 def test_root_must_return_200(client):
     response = client.get('/')
 
@@ -14,5 +17,66 @@ def test_create_user(client):
             'password': 'secret123',
         },
     )
+
     assert response.status_code == 201
-    assert response.json() == {'username': 'foo', 'email': 'foo@bar.com'}
+    assert response.json() == {
+        'id': 1,
+        'username': 'foo',
+        'email': 'foo@bar.com',
+    }
+
+
+def test_get_users(client):
+    response = client.get('/users/')
+    assert response.status_code == 200
+    assert response.json() == {'users': []}
+
+
+def test_get_users_with_created_users_on_db(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bar',
+            'email': 'bar@foo.com',
+            'password': 'anothersecret',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'bar',
+        'email': 'bar@foo.com',
+        'id': 1,
+    }
+
+
+def test_update_user_when_not_exists(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bar',
+            'email': 'bar@foo.com',
+            'password': 'anothersecret',
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'user not found'}
+
+
+def test_delete_user(client, user):
+    response = client.delete('/users/1')
+    assert response.status_code == 200
+    assert response.json() == {'detail': 'user deleted'}
+
+
+def test_delete_user_when_not_exists(client):
+    response = client.delete(
+        '/users/1',
+    )
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'user not found'}
