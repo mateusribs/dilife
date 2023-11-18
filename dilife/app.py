f0ffaa1205134665dcf244ff0e4817e472050a1e
Mateus Ribeiro <mateus.ribe.sva@gmail.com>
2023-11-18 15:08:40 -0300

    refactor: included routers on main app

--- dilife/app.py
+++ dilife/app.py
@@ -1,75 +1,13 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy import select
-from sqlalchemy.orm import Session
-from starlette.types import Message
+from fastapi import FastAPI
 
-from dilife.database import get_session
-from dilife.models import User
-from dilife.schemas import UserList, UserPublic, UserSchema
+from dilife.routes import auth, users
 
 app = FastAPI()
+
+app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Hello World'}
-
-
-@app.post('/users/', status_code=201, response_model=UserPublic)
-def create_user(user: UserSchema, session: Session = Depends(get_session)):
-    db_user = session.scalar(
-        select(User).where(User.username == user.username)
-    )
-
-    if db_user:
-        raise HTTPException(
-            status_code=400, detail='Username already registered'
-        )
-
-    db_user = User(
-        username=user.username, password=user.password, email=user.email
-    )
-
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
-    return db_user
-
-
-@app.get('/users/', response_model=UserList)
-def get_users(
-    skip: int = 0, limit: int = 10, session: Session = Depends(get_session)
-):
-    users = session.scalars(select(User).offset(skip).limit(limit)).all()
-    return {'users': users}
-
-
-@app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(
-    user_id: int, user: UserSchema, session: Session = Depends(get_session)
-):
-    db_user = session.scalar(select(User).where(User.id == user_id))
-
-    if db_user is None:
-        raise HTTPException(status_code=404, detail='user not found')
-
-    db_user.username = user.username
-    db_user.password = user.password
-    db_user.email = user.email
-    session.commit()
-    session.refresh(db_user)
-
-    return db_user
-
-
-@app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int, session: Session = Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.id == user_id))
-
-    if db_user is None:
-        raise HTTPException(status_code=404, detail='user not found')
-
-    session.delete(db_user)
-    session.commit()
-
-    return {'detail': 'user deleted'}
