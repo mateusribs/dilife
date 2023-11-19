1b0c266681baf75b1b4a0bc540afc21463c47aba
Mateus Ribeiro <mateus.ribe.sva@gmail.com>
2023-11-19 18:02:17 -0300

    feature: included credit cards route into main app

--- dilife/app.py
+++ dilife/app.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
-from dilife.routes import accounts, auth, users
+from dilife.routes import accounts, auth, credit_cards, users
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(accounts.router)
+app.include_router(credit_cards.router)
 
 
 @app.get('/')
